71dcc884307fbc9bf9e93cf1964f065165b5782d
Tom McAdam <mcadam1980@gmail.com>
2026-05-24 16:48:59 +0900

    Fix for build bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    ...(process.env.NODE_ENV !== 'test' && { '@tailwindcss/postcss': {} }),
     autoprefixer: {},
   },
 }
